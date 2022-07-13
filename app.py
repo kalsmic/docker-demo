e9c86cbc85b7f9b7b95bc28558e7780bd82eabc9
Arthur <kalulearthur@gmail.com>
2022-07-13 12:24:48 +0000

    add host

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
 if __name__ == "__main__":
-	app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
