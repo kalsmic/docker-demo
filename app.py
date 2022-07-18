cea46c19fd1f1f1d06ae42b13617dfb5681f98e2
Arthur <kalulearthur@gmail.com>
2022-07-18 03:57:44 +0000

    dynamically change debug mode

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+    
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    debug = os.environ.get("FLASK_DEBUG", False)
+    app.run(debug=debug,host='0.0.0.0',port=port)
